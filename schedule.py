49df5f464af6bbc4f0d273096be25f6c91f544d1
Grace Milton <gmmilton10@gmail.com>
2017-03-13 11:47:30 -0400

    Added nickname feature to schedule.py

--- schedule.py
+++ schedule.py
@@ -21,6 +21,12 @@ SEASON_COUNT3=1
 SEASON_COUNT4=1
 SEASON_COUNT5=1
 
+NICKNAME_ONE=''
+NICKNAME_TWO=''
+NICKNAME_THREE=''
+NICKNAME_FOUR=''
+NICKNAME_FIVE=''
+
 EPISODE_COUNT1=1
 EPISODE_COUNT2=1
 EPISODE_COUNT3=1
@@ -38,9 +44,11 @@ MAX=0
 # Usage Function
 
 def usage(status=0):
-   print '''Usage: {} [ -n NUM_ENTRIES -b BOOK ] ENTRY_ONE SEASON_COUNT1 ENTRY_TWO SEASON_COUNT2...
+   print '''Usage: {} [ -n NUM_ENTRIES -b BOOK ] ENTRY_ONE [ -s SEASON -x NICKNAME ] ENTRY_TWO [ -s SEASON -x NICKNAME ]...
    -n	NUM_ENTRIES	Number of TV shows/movies to sort (default: 1)
-   -b	BOOK		Includes 1 book in schedule'''.format(os.path.basename(sys.argv[0]))
+   -b	BOOK		Includes 1 book in schedule
+   -s	SEASON		Which season to call for a show (dafult: 1)
+   -a	NICKNAME	Creates a alternative name for the show that can be displayed in lieu of the title'''.format(os.path.basename(sys.argv[0]))
    sys.exit(status)
 
 # Parse command line options
@@ -59,33 +67,81 @@ while len(args) and args[0].startswith('-') and len(args[0]) > 1:
    else:
       usage(1)
 
+if len(args) < 1:
+   usage(1)
+
 ## Set entries for TV shows/movies to include
 
 if len(args) >= 1:
    arg = args.pop(0)
    ENTRY_ONE = arg
-   arg = args.pop(0)
-   SEASON_COUNT1 = int(arg)
+   NICKNAME_ONE = arg
+   while len(args) and args[0].startswith('-') and len(args[0]) > 1:
+      arg = args.pop(0)
+      if arg == '-s':
+         arg = args.pop(0)
+         SEASON_COUNT1 = int(arg)
+      elif arg == '-a':
+         arg = args.pop(0)
+         NICKNAME_ONE = arg
+      else:
+         usage(1)
 if len(args) >= 1:
    arg = args.pop(0)
    ENTRY_TWO = arg
-   arg = args.pop(0)
-   SEASON_COUNT2 = int(arg)
+   NICKNAME_TWO = arg
+   while len(args) and args[0].startswith('-') and len(args[0]) > 1:
+      arg = args.pop(0)
+      if arg == '-s':
+         arg = args.pop(0)
+         SEASON_COUNT2 = int(arg)
+      elif arg == '-a':
+         arg = args.pop(0)
+         NICKNAME_TWO = arg
+      else:
+         usage(1)
 if len(args) >= 1:
    arg = args.pop(0)
    ENTRY_THREE = arg
-   arg = args.pop(0)
-   SEASON_COUNT3 = int(arg)
+   NICKNAME_THREE = arg
+   while len(args) and args[0].startswith('-') and len(args[0]) > 1:
+      arg = args.pop(0)
+      if arg == '-s':
+         arg = args.pop(0)
+         SEASON_COUNT3 = int(arg)
+      elif arg == '-a':
+         arg = args.pop(0)
+         NICKNAME_THREE = arg
+      else:
+         usage(1)
 if len(args) >= 1:
    arg = args.pop(0)
    ENTRY_FOUR = arg
-   arg = args.pop(0)
-   SEASON_COUNT4 = int(arg)
+   NICKNAME_FOUR = arg
+   while len(args) and args[0].startswith('-') and len(args[0]) > 1:
+      arg = args.pop(0)
+      if arg == '-s':
+         arg = args.pop(0)
+         SEASON_COUNT4 = int(arg)
+      elif arg == '-a':
+         arg = args.pop(0)
+         NICKNAME_FOUR = arg
+      else:
+         usage(1)
 if len(args) >= 1:
    arg = args.pop(0)
    ENTRY_FIVE = arg
-   arg = args.pop(0)
-   SEASON_COUNT5 = int(arg)
+   NICKNAME_FIVE = arg
+   while len(args) and args[0].startswith('-') and len(args[0]) > 1:
+      arg = args.pop(0)
+      if arg == '-s':
+         arg = args.pop(0)
+         SEASON_COUNT5 = int(arg)
+      elif arg == '-a':
+         arg = args.pop(0)
+         NICKNAME_FIVE = arg
+      else:
+         usage(1)
 
 # Main Execution
 
@@ -118,34 +174,34 @@ MAX = max(EPISODE_MAX1, EPISODE_MAX2, EPISODE_MAX3, EPISODE_MAX4, EPISODE_MAX5)
 ## Iterate through and print alternating episodes of each season
 for x in xrange(1, MAX+1):
    if x <= EPISODE_MAX1 and NUM_ENTRIES > 1 and (x <= EPISODE_MAX2 or x <= EPISODE_MAX3 or x <= EPISODE_MAX4 or x <= EPISODE_MAX5):
-      print req_one.json()['Title']+'['+str(SEASON_COUNT1)+'.'+str(EPISODE_COUNT1)+']','-',
+      print NICKNAME_ONE+'['+str(SEASON_COUNT1)+'.'+str(EPISODE_COUNT1)+']','-',
       EPISODE_COUNT1 = EPISODE_COUNT1 + 1
    elif x == EPISODE_MAX1 or NUM_ENTRIES == 1:
-      print req_one.json()['Title']+'['+str(SEASON_COUNT1)+'.'+str(EPISODE_COUNT1)+']'
+      print NICKNAME_ONE+'['+str(SEASON_COUNT1)+'.'+str(EPISODE_COUNT1)+']'
       EPISODE_COUNT1 = EPISODE_COUNT1 + 1
 
    if NUM_ENTRIES >= 2 and x <= EPISODE_MAX2 and (x <= EPISODE_MAX3 or x <= EPISODE_MAX4 or x <= EPISODE_MAX5):
-      print req_two.json()['Title']+'['+str(SEASON_COUNT2)+'.'+str(EPISODE_COUNT2)+']','-',
+      print NICKNAME_TWO+'['+str(SEASON_COUNT2)+'.'+str(EPISODE_COUNT2)+']','-',
       EPISODE_COUNT2 = EPISODE_COUNT2 + 1
    elif NUM_ENTRIES >= 2 and x <= EPISODE_MAX2:
-      print req_two.json()['Title']+'['+str(SEASON_COUNT2)+'.'+str(EPISODE_COUNT2)+']'
+      print NICKNAME_TWO+'['+str(SEASON_COUNT2)+'.'+str(EPISODE_COUNT2)+']'
       EPISODE_COUNT2 = EPISODE_COUNT2 + 1
 
    if NUM_ENTRIES >= 3 and x <= EPISODE_MAX3 and (x <= EPISODE_MAX4 or x <= EPISODE_MAX5):
-      print req_three.json()['Title']+'['+str(SEASON_COUNT3)+'.'+str(EPISODE_COUNT3)+']','-',
+      print NICKNAME_THREE+'['+str(SEASON_COUNT3)+'.'+str(EPISODE_COUNT3)+']','-',
       EPISODE_COUNT3 = EPISODE_COUNT3 + 1
    elif NUM_ENTRIES >= 3 and x <= EPISODE_MAX3:
-      print req_three.json()['Title']+'['+str(SEASON_COUNT3)+'.'+str(EPISODE_COUNT3)+']'
+      print NICKNAME_THREE+'['+str(SEASON_COUNT3)+'.'+str(EPISODE_COUNT3)+']'
       EPISODE_COUNT3 = EPISODE_COUNT3 + 1
 
    if NUM_ENTRIES >= 4 and x <= EPISODE_MAX4 and x <= EPISODE_MAX5:
-      print req_four.json()['Title']+'['+str(SEASON_COUNT4)+'.'+str(EPISODE_COUNT4)+']','-',
+      print NICKNAME_FOUR+'['+str(SEASON_COUNT4)+'.'+str(EPISODE_COUNT4)+']','-',
       EPISODE_COUNT4 = EPISODE_COUNT4 + 1
    elif NUM_ENTRIES >= 4 and x <= EPISODE_MAX4:
-      print req_four.json()['Title']+'['+str(SEASON_COUNT4)+'.'+str(EPISODE_COUNT4)+']'
+      print NICKNAME_FOUR+'['+str(SEASON_COUNT4)+'.'+str(EPISODE_COUNT4)+']'
       EPISODE_COUNT4 = EPISODE_COUNT4 + 1
 
    if NUM_ENTRIES >= 5 and x <= EPISODE_MAX5:
-      print req_five.json()['Title']+'['+str(SEASON_COUNT5)+'.'+str(EPISODE_COUNT5)+']'
+      print NICKNAME_FIVE+'['+str(SEASON_COUNT5)+'.'+str(EPISODE_COUNT5)+']'
       EPISODE_COUNT5 = EPISODE_COUNT5 + 1
 
