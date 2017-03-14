7db16f693395af09e85dc178cd6e82720915c2b4
Grace Milton <gmmilton10@gmail.com>
2017-03-14 15:09:38 -0400

    Adding lists to schedule.py

--- schedule.py
+++ schedule.py
@@ -8,41 +8,15 @@ import re
 # Global Variables
 
 NUM_ENTRIES=1
-ENTRY_ONE='Stargate+SG-1'
-ENTRY_TWO=''
-ENTRY_THREE=''
-ENTRY_FOUR=''
-ENTRY_FIVE=''
+ENTRY_LIST = ['', '', '', '', '']
 BOOK=''
-
-SEASON_COUNT1=1
-SEASON_COUNT2=1
-SEASON_COUNT3=1
-SEASON_COUNT4=1
-SEASON_COUNT5=1
-
-NICKNAME_ONE=''
-NICKNAME_TWO=''
-NICKNAME_THREE=''
-NICKNAME_FOUR=''
-NICKNAME_FIVE=''
-
-EPISODE_COUNT1=1
-EPISODE_COUNT2=1
-EPISODE_COUNT3=1
-EPISODE_COUNT4=1
-EPISODE_COUNT5=1
-
-EPISODE_MAX1=0
-EPISODE_MAX2=0
-EPISODE_MAX3=0
-EPISODE_MAX4=0
-EPISODE_MAX5=0
-
+SEASON_LIST = [1, 1, 1, 1, 1]
+NICKNAME_LIST = ['','','','','']
+EPISODE_LIST = [1, 1, 1, 1, 1]
+MAX_LIST = [0, 0, 0, 0, 0]
 MAX=0
-
 RED='\033[0;31m'
-
+REQ_LIST = [{}, {}, {}, {}, {}]
 # Usage Function
 
 def usage(status=0):
@@ -96,178 +70,159 @@ if len(args) < 1:
 
 if len(args) >= 1:
    arg = args.pop(0)
-   ENTRY_ONE = arg
-   NICKNAME_ONE = arg
+   ENTRY_LIST[0] = arg
+   NICKNAME_LIST[0] = arg
    while len(args) and args[0].startswith('-') and len(args[0]) > 1:
       arg = args.pop(0)
       if arg == '-s':
          arg = args.pop(0)
-         SEASON_COUNT1 = int(arg)
+         SEASON_LIST[0] = int(arg)
       elif arg == '-a':
          arg = args.pop(0)
-         NICKNAME_ONE = arg
+         NICKNAME_LIST[0] = arg
       else:
          usage(1)
 if len(args) >= 1:
    arg = args.pop(0)
-   ENTRY_TWO = arg
-   NICKNAME_TWO = arg
+   ENTRY_LIST[1] = arg
+   NICKNAME_LIST[1] = arg
    while len(args) and args[0].startswith('-') and len(args[0]) > 1:
       arg = args.pop(0)
       if arg == '-s':
          arg = args.pop(0)
-         SEASON_COUNT2 = int(arg)
+         SEASON_LIST[1] = int(arg)
       elif arg == '-a':
          arg = args.pop(0)
-         NICKNAME_TWO = arg
+         NICKNAME_LIST[1] = arg
       else:
          usage(1)
 if len(args) >= 1:
    arg = args.pop(0)
-   ENTRY_THREE = arg
-   NICKNAME_THREE = arg
+   ENTRY_LIST[2] = arg
+   NICKNAME_LIST[2] = arg
    while len(args) and args[0].startswith('-') and len(args[0]) > 1:
       arg = args.pop(0)
       if arg == '-s':
          arg = args.pop(0)
-         SEASON_COUNT3 = int(arg)
+         SEASON_LIST[2] = int(arg)
       elif arg == '-a':
          arg = args.pop(0)
-         NICKNAME_THREE = arg
+         NICKNAME_LIST[2] = arg
       else:
          usage(1)
 if len(args) >= 1:
    arg = args.pop(0)
-   ENTRY_FOUR = arg
-   NICKNAME_FOUR = arg
+   ENTRY_LIST[3] = arg
+   NICKNAME_LIST[3] = arg
    while len(args) and args[0].startswith('-') and len(args[0]) > 1:
       arg = args.pop(0)
       if arg == '-s':
          arg = args.pop(0)
-         SEASON_COUNT4 = int(arg)
+         SEASON_LIST[3] = int(arg)
       elif arg == '-a':
          arg = args.pop(0)
-         NICKNAME_FOUR = arg
+         NICKNAME_LIST[3] = arg
       else:
          usage(1)
 if len(args) >= 1:
    arg = args.pop(0)
-   ENTRY_FIVE = arg
-   NICKNAME_FIVE = arg
+   ENTRY_LIST[4] = arg
+   NICKNAME_LIST[4] = arg
    while len(args) and args[0].startswith('-') and len(args[0]) > 1:
       arg = args.pop(0)
       if arg == '-s':
          arg = args.pop(0)
-         SEASON_COUNT5 = int(arg)
+         SEASON_LIST[4] = int(arg)
       elif arg == '-a':
          arg = args.pop(0)
-         NICKNAME_FIVE = arg
+         NICKNAME_LIST[4] = arg
       else:
          usage(1)
 
 # Main Execution
 
-## Request info about each show
+## Request info about each show and set number of episodes for each show
 url = 'http://www.omdbapi.com/?'
-req_one = requests.get(url+'t='+ENTRY_ONE+'&Season='+str(SEASON_COUNT1))
-if NUM_ENTRIES >= 2:
-   req_two = requests.get(url+'t='+ENTRY_TWO+'&Season='+str(SEASON_COUNT2))
-if NUM_ENTRIES >= 3:
-   req_three = requests.get(url+'t='+ENTRY_THREE+'&Season='+str(SEASON_COUNT3))
-if NUM_ENTRIES >= 4:
-   req_four = requests.get(url+'t='+ENTRY_FOUR+'&Season='+str(SEASON_COUNT4))
-if NUM_ENTRIES >= 5:
-   req_five = requests.get(url+'t='+ENTRY_FIVE+'&Season='+str(SEASON_COUNT5))
-
-## Set number of episodes for each show
-EPISODE_MAX1 = max([int(x['Episode']) for x in req_one.json()['Episodes']])
-if NUM_ENTRIES >= 2:
-   EPISODE_MAX2 = max([int(x['Episode']) for x in req_two.json()['Episodes']])
-if NUM_ENTRIES >= 3:
-   EPISODE_MAX3 = max([int(x['Episode']) for x in req_three.json()['Episodes']])
-if NUM_ENTRIES >= 4:
-   EPISODE_MAX4 = max([int(x['Episode']) for x in req_four.json()['Episodes']])
-if NUM_ENTRIES >= 5:
-   EPISODE_MAX5 = max([int(x['Episode']) for x in req_five.json()['Episodes']])
+i = 0
+while i < NUM_ENTRIES:
+   REQ_LIST[i] = requests.get(url+'t='+ENTRY_LIST[i]+'&Season='+str(SEASON_LIST[i]))
+   MAX_LIST[i] = max([int(x['Episode']) for x in REQ_LIST[i].json()['Episodes']])
+   i = i + 1
 
 ## Find largest amount of episodes in a season
-MAX = max(EPISODE_MAX1, EPISODE_MAX2, EPISODE_MAX3, EPISODE_MAX4, EPISODE_MAX5)
+MAX = max(MAX_LIST)
 
 ## Iterate through and print alternating episodes of each season
 for x in xrange(1, MAX+1):
 
-   if NUM_ENTRIES == 1:
-      print_last(NICKNAME_ONE, SEASON_COUNT1, EPISODE_COUNT1)
-      EPISODE_COUNT1 = EPISODE_COUNT1 + 1 
-   else:
-      if x < EPISODE_MAX1:
-         if x <= EPISODE_MAX2 or x <= EPISODE_MAX3 or x <= EPISODE_MAX4 or x <= EPISODE_MAX5:
-            print_episode(NICKNAME_ONE, SEASON_COUNT1, EPISODE_COUNT1)
-            EPISODE_COUNT1 = EPISODE_COUNT1 + 1
-         else:
-            print_last(NICKNAME_ONE, SEASON_COUNT1, EPISODE_COUNT1)
-            EPISODE_COUNT1 = EPISODE_COUNT1 + 1
-      elif x == EPISODE_MAX1: 
-         if x <= EPISODE_MAX2 or x <= EPISODE_MAX3 or x <= EPISODE_MAX4 or x <= EPISODE_MAX5:
-            print_color(NICKNAME_ONE, SEASON_COUNT1, EPISODE_COUNT1, RED)
-            EPISODE_COUNT1 = EPISODE_COUNT1 + 1
-         else:
-            print_last_color(NICKNAME_ONE, SEASON_COUNT1, EPISODE_COUNT1, RED)
-            EPISODE_COUNT1 = EPISODE_COUNT1 + 1
+   if x < MAX_LIST[0]:
+      if x <= MAX_LIST[1] or x <= MAX_LIST[2] or x <= MAX_LIST[3] or x <= MAX_LIST[4]:
+         print_episode(NICKNAME_LIST[0], SEASON_LIST[0], EPISODE_LIST[0])
+         EPISODE_LIST[0] = EPISODE_LIST[0] + 1
+      else:
+         print_last(NICKNAME_LIST[0], SEASON_LIST[0], EPISODE_LIST[0])
+         EPISODE_LIST[0] = EPISODE_LIST[0] + 1
+   elif x == MAX_LIST[0]:
+      if x <= MAX_LIST[1] or x <= MAX_LIST[2] or x <= MAX_LIST[3] or x <= MAX_LIST[4]:
+         print_color(NICKNAME_LIST[0], SEASON_LIST[0], EPISODE_LIST[0], RED)
+         EPISODE_LIST[0] = EPISODE_LIST[0] + 1
+      else:
+         print_last_color(NICKNAME_LIST[0], SEASON_LIST[0], EPISODE_LIST[0], RED)
+         EPISODE_LIST[0] = EPISODE_LIST[0] + 1
 
    if NUM_ENTRIES >= 2:
-      if x < EPISODE_MAX2:
-         if x <= EPISODE_MAX3 or x <= EPISODE_MAX4 or x <= EPISODE_MAX5:
-            print_episode(NICKNAME_TWO, SEASON_COUNT2, EPISODE_COUNT2)
-            EPISODE_COUNT2 = EPISODE_COUNT2 + 1
+      if x < MAX_LIST[1]:
+         if x <= MAX_LIST[2] or x <= MAX_LIST[3] or x <= MAX_LIST[4]:
+            print_episode(NICKNAME_LIST[1], SEASON_LIST[1], EPISODE_LIST[1])
+            EPISODE_LIST[1] = EPISODE_LIST[1] + 1
          else:
-            print_last(NICKNAME_TWO, SEASON_COUNT2, EPISODE_COUNT2)
-            EPISODE_COUNT2 = EPISODE_COUNT2 + 1
-      elif x == EPISODE_MAX2:
-         if x <= EPISODE_MAX3 or x <= EPISODE_MAX4 or x <= EPISODE_MAX5:
-            print_color(NICKNAME_TWO, SEASON_COUNT2, EPISODE_COUNT2, RED)
-            EPISODE_COUNT2 = EPISODE_COUNT2 + 1
+            print_last(NICKNAME_LIST[1], SEASON_LIST[1], EPISODE_LIST[1])
+            EPISODE_LIST[1] = EPISODE_LIST[1] + 1
+      elif x == MAX_LIST[1]:
+         if x <= MAX_LIST[2] or x <= MAX_LIST[3] or x <= MAX_LIST[4]:
+            print_color(NICKNAME_LIST[1], SEASON_LIST[1], EPISODE_LIST[1], RED)
+            EPISODE_LIST[1] = EPISODE_LIST[1] + 1
          else:
-            print_last_color(NICKNAME_TWO, SEASON_COUNT2, EPISODE_COUNT2, RED)
-            EPISODE_COUNT2 = EPISODE_COUNT2 + 1
+            print_last_color(NICKNAME_LIST[1], SEASON_LIST[1], EPISODE_LIST[1], RED)
+            EPISODE_LIST[1] = EPISODE_LIST[1] + 1
 
    if NUM_ENTRIES >= 3:
-      if x < EPISODE_MAX3:
-         if x <= EPISODE_MAX4 or x <= EPISODE_MAX5:
-            print_episode(NICKNAME_THREE, SEASON_COUNT3, EPISODE_COUNT3)
-            EPISODE_COUNT3 = EPISODE_COUNT3 + 1
+      if x < MAX_LIST[2]:
+         if x <= MAX_LIST[3] or x <= MAX_LIST[4]:
+            print_episode(NICKNAME_LIST[2], SEASON_LIST[2], EPISODE_LIST[2])
+            EPISODE_LIST[2] = EPISODE_LIST[2] + 1
          else:
-            print_last(NICKNAME_THREE, SEASON_COUNT3, EPISODE_COUNT3)
-            EPISODE_COUNT3 = EPISODE_COUNT3 + 1
-      elif x == EPISODE_MAX3:
-         if x <= EPISODE_MAX4 or x <= EPISODE_MAX5:
-            print_color(NICKNAME_THREE, SEASON_COUNT3, EPISODE_COUNT3, RED)
-            EPISODE_COUNT3 = EPISODE_COUNT3 + 1
+            print_last(NICKNAME_LIST[2], SEASON_LIST[2], EPISODE_LIST[2])
+            EPISODE_LIST[2] = EPISODE_LIST[2] + 1
+      elif x == MAX_LIST[2]:
+         if x <= MAX_LIST[3] or x <= MAX_LIST[4]:
+            print_color(NICKNAME_LIST[2], SEASON_LIST[2], EPISODE_LIST[2], RED)
+            EPISODE_LIST[2] = EPISODE_LIST[2] + 1
          else:
-            print_last_color(NICKNAME_THREE, SEASON_COUNT3, EPISODE_COUNT3, RED)
-            EPISODE_COUNT3 = EPISODE_COUNT3 + 1
+            print_last_color(NICKNAME_LIST[2], SEASON_LIST[2], EPISODE_LIST[2], RED)
+            EPISODE_LIST[2] = EPISODE_LIST[2] + 1
 
    if NUM_ENTRIES >= 4:
-      if x < EPISODE_MAX4:
-         if x <= EPISODE_MAX5:
-            print_episode(NICKNAME_FOUR, SEASON_COUNT4, EPISODE_COUNT4)
-            EPISODE_COUNT4 = EPISODE_COUNT4 + 1
+      if x < MAX_LIST[3]:
+         if x <= MAX_LIST[4]:
+            print_episode(NICKNAME_LIST[3], SEASON_LIST[3], EPISODE_LIST[3])
+            EPISODE_LIST[3] = EPISODE_LIST[3] + 1
          else:
-            print_last(NICKNAME_FOUR, SEASON_COUNT4, EPISODE_COUNT4)
-            EPISODE_COUNT4 = EPISODE_COUNT4 + 1
-      elif x == EPISODE_MAX4:
-         if x <= EPISODE_MAX5:
-            print_color(NICKNAME_FOUR, SEASON_COUNT4, EPISODE_COUNT4, RED)
-            EPISODE_COUNT4 = EPISODE_COUNT4 + 1
+            print_last(NICKNAME_LIST[3], SEASON_LIST[3], EPISODE_LIST[3])
+            EPISODE_LIST[3] = EPISODE_LIST[3] + 1
+      elif x == MAX_LIST[3]:
+         if x <= MAX_LIST[4]:
+            print_color(NICKNAME_LIST[3], SEASON_LIST[3], EPISODE_LIST[3], RED)
+            EPISODE_LIST[3] = EPISODE_LIST[3] + 1
          else:
-            print_last_color(NICKNAME_FOUR, SEASON_COUNT4, EPISODE_COUNT4, RED)
-            EPISODE_COUNT4 = EPISODE_COUNT4 + 1
+            print_last_color(NICKNAME_LIST[3], SEASON_LIST[3], EPISODE_LIST[3], RED)
+            EPISODE_LIST[3] = EPISODE_LIST[3] + 1
 
    if NUM_ENTRIES >= 5:
-      if x < EPISODE_MAX5:
-         print_last(NICKNAME_FIVE, SEASON_COUNT5, EPISODE_COUNT5)
-         EPISODE_COUNT5 = EPISODE_COUNT5 + 1
-      elif x == EPISODE_MAX5:
-         print_last_color(NICKNAME_FIVE, SEASON_COUNT5, EPISODE_COUNT5, RED)
-         EPISODE_COUNT5 = EPISODE_COUNT5 + 1
+      if x < MAX_LIST[4]:
+         print_last(NICKNAME_LIST[4], SEASON_LIST[4], EPISODE_LIST[4])
+         EPISODE_LIST[4] = EPISODE_LIST[4] + 1
+      elif x == MAX_LIST[4]:
+         print_last_color(NICKNAME_LIST[4], SEASON_LIST[4], EPISODE_LIST[4], RED)
+         EPISODE_LIST[4] = EPISODE_LIST[4] + 1
 
