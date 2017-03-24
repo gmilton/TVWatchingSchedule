83d22a571092ae4e95007d652487c9cc770ed36c
Grace Milton <gmmilton10@gmail.com>
2017-03-24 11:44:04 -0400

    Adding watched feature

--- schedule.py
+++ schedule.py
@@ -11,9 +11,11 @@ BOOK=''
 SEASON_LIST = [1, 1, 1, 1, 1]
 NICKNAME_LIST = ['','','','','']
 EPISODE_LIST = [1, 1, 1, 1, 1]
+WATCHED_LIST = [0, 0, 0, 0, 0]
 MAX_LIST = [0, 0, 0, 0, 0]
 MAX=0
 RED='\033[0;31m'
+DEFAULT_COLOR='\033[38;5;247m'
 REQ_LIST = [{}, {}, {}, {}, {}]
 
 # Usage Function
@@ -22,24 +24,32 @@ def usage(status=0):
    -n	NUM_ENTRIES	Number of TV shows/movies to sort (default: 1)
    -b	BOOK		Includes 1 book in schedule (FEATURE NOT IMPLEMENTED YET)
    -s	SEASON		Which season to call for a show (default: 1)
-   -a	NICKNAME	Creates a alternative name for the show that can be displayed in lieu of the title'''.format(os.path.basename(sys.argv[0]))
+   -a	NICKNAME	Creates a alternative name for the show that can be displayed in lieu of the title
+   -w	WATCHED		Marks number of episodes already watched and removes from list'''.format(os.path.basename(sys.argv[0]))
    sys.exit(status)
 
 # Print Function
 def print_episode(INDEX):
-   print NICKNAME_LIST[INDEX]+'['+str(SEASON_LIST[INDEX])+'.'+str(EPISODE_LIST[INDEX])+']','-',
+   print DEFAULT_COLOR+NICKNAME_LIST[INDEX]+'['+str(SEASON_LIST[INDEX])+'.'+str(EPISODE_LIST[INDEX])+']','-',
 
 # Print in Color Function
 def print_color(INDEX, COLOR):
-   print COLOR+NICKNAME_LIST[INDEX]+'['+str(SEASON_LIST[INDEX])+'.'+str(EPISODE_LIST[INDEX])+']'+'\033[38;5;247m','-',
+   print COLOR+NICKNAME_LIST[INDEX]+'['+str(SEASON_LIST[INDEX])+'.'+str(EPISODE_LIST[INDEX])+']'+DEFAULT_COLOR,'-',
 
 # Print Last Function
 def print_last(INDEX):
-   print NICKNAME_LIST[INDEX]+'['+str(SEASON_LIST[INDEX])+'.'+str(EPISODE_LIST[INDEX])+']'
+   print DEFAULT_COLOR+NICKNAME_LIST[INDEX]+'['+str(SEASON_LIST[INDEX])+'.'+str(EPISODE_LIST[INDEX])+']'
 
 # Print Last Line in Color Function
 def print_last_color(INDEX, COLOR):
-   print COLOR+NICKNAME_LIST[INDEX]+'['+str(SEASON_LIST[INDEX])+'.'+str(EPISODE_LIST[INDEX])+']'+'\033[38;5;247m'
+   print COLOR+NICKNAME_LIST[INDEX]+'['+str(SEASON_LIST[INDEX])+'.'+str(EPISODE_LIST[INDEX])+']'+DEFAULT_COLOR
+
+# Returns 1 if show is not last in row
+def show_to_right(CURRENT_EP, INDEX):
+   if CURRENT_EP <= max([MAX_LIST[j] for j in xrange(INDEX+1, NUM_ENTRIES+1)]) and CURRENT_EP > min([WATCHED_LIST[k] for k in xrange(INDEX+1, NUM_ENTRIES)]):
+      return 1
+   else:
+      return 0
 
 # Parse command line options
 args = sys.argv[1:]
@@ -73,6 +83,9 @@ for i in xrange(0, NUM_ENTRIES):
          elif arg == '-a':
             arg = args.pop(0)
             NICKNAME_LIST[i] = arg
+	 elif arg == '-w':
+	    arg = args.pop(0)
+	    WATCHED_LIST[i] = int(arg)
          else:
             usage(1)
 
@@ -93,18 +106,20 @@ MAX = max(MAX_LIST)
 ## Iterate through and print alternating episodes of each season
 for x in xrange(1, MAX+1):
    for i in xrange(0, NUM_ENTRIES):
-      if x < MAX_LIST[i]:
-         if x <= max([MAX_LIST[j] for j in xrange(i+1, NUM_ENTRIES+1)]):
-            print_episode(i)
+      if x < MAX_LIST[i] and x > WATCHED_LIST[i]:
+         if show_to_right(x, i):
+	    print_episode(i)
             EPISODE_LIST[i] = EPISODE_LIST[i] + 1
          else:
             print_last(i)
             EPISODE_LIST[i] = EPISODE_LIST[i] + 1
-      elif x == MAX_LIST[i]:
-         if x <= max([MAX_LIST[j] for j in xrange(i+1, NUM_ENTRIES+1)]):
+      elif x == MAX_LIST[i] and x > WATCHED_LIST[i]:
+         if show_to_right(x,i):
             print_color(i, RED)
             EPISODE_LIST[i] = EPISODE_LIST[i] + 1
          else:
             print_last_color(i, RED)
             EPISODE_LIST[i] = EPISODE_LIST[i] + 1
+      elif x <= WATCHED_LIST[i]:
+         EPISODE_LIST[i] = EPISODE_LIST[i] + 1
 
