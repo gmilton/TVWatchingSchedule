5983f5d4bcfe4615fa310082e7ef999e40100074
Grace Milton <gmmilton10@gmail.com>
2017-04-28 10:52:42 -0400

    Updating column1.sh

--- column1.sh
+++ column1.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# column1.sh
 
-./schedule.py -n 5 'Stargate SG-1' -s 4 -a 'SG-1' 'Hawaii Five-0' -s 6 -a 'H50' 'Merlin' 'Luke Cage' -a 'Luke' 'Robin Hood' -a 'Robin'
+./schedule.py -n 5 'Stargate SG-1' -s 5 -a 'SG-1' -w 4 'New Girl' -s 5 -a 'NG' -w 1 'Merlin' -s 2 "13 Reasons Why" -a '13RW' 'Robin Hood' -s 2 -a 'Robin'
 
