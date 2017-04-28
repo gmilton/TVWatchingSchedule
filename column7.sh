2f4212f92c7518773cc3f64416699b8ad304ee27
Grace Milton <gmmilton10@gmail.com>
2017-04-28 10:53:24 -0400

    Updating column7.sh

--- column7.sh
+++ column7.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# column7.sh
+#column7.sh
 
 ./schedule.py -n 5 'Stargate Universe' -a 'SGU' 'Supernatural' -s 11 -a 'SPN' 'Sanctuary' 'Battlestar Galactica' -s 3 -a 'BSG' 'Star Trek: Deep Space Nine' -s 5 -a 'DS9'
 
