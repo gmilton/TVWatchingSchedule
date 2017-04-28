5cb39ef137d93e3a572cb7cf32d2b2159bf5235f
Grace Milton <gmmilton10@gmail.com>
2017-04-28 10:53:03 -0400

    Updating column6.sh

--- column6.sh
+++ column6.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# column7.sh
 
-./schedule.py -n 5 'Stargate SG-1' -s 9 -a 'SG-1' 'Daredevil'  -a 'DD' 'Sanctuary' 'The IT Crowd' -s 3 -a 'IT' 'Star Trek: Deep Space Nine' -s 3 -a 'DS9'
+./schedule.py -n 5 'Stargate SG-1' -s 10 -a 'SG-1' 'Gotham' -s 2 'Galavant' 'Battlestar Galactica' -s 2 -a 'BSG' 'Star Trek: Deep Space Nine' -s 4 -a 'DS9'
 
