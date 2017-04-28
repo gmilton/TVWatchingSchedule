470db8426febe6b5a35d4a423b7438663e834940
Grace Milton <gmmilton10@gmail.com>
2017-04-28 10:52:51 -0400

    Updating column3.sh

--- column3.sh
+++ column3.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# column3.sh
 
-./schedule.py -n 5 'Stargate SG-1' -s 6 -a 'SG-1' 'The Great British Baking Show' -s 6 -a 'Bake-Off' 'Merlin' -s 3 'Supergirl' -a 'SGirl' 'Robin Hood' -s 3 -a 'Robin'
+./schedule.py -n 5 'Stargate SG-1' -s 7 -a 'SG-1' 'The Great British Baking Show' -s 2 -a 'Bake-Off: Masterclass' 'Merlin' -s 4 'Legends of Tomorrow' -s 2 -a 'LoT' 'Star Trek: Deep Space Nine' -s 1 -a 'DS9'
 
